54572a10b63d43be07fdd3051e1be66615ad5d4d
chenjun <chenjun8@asiainfo.com>
2020-09-10 21:07:03 +0800

    SQL 文件提交

--- src/main/resources/sql/SQLTimplate.sql
+++ src/main/resources/sql/SQLTimplate.sql
@@ -18,3 +18,21 @@ CREATE TABLE `db_country_land` (
   UNIQUE KEY `idx_enterprise_code` (`ENTERPRISE_CODE`),
   KEY `idx_enterprise_name_createtime` (`CREATE_DATE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='记录企业国土数据资源数据信息';
+
+-- 国税数据导入模型
+DROP TABLE IF EXISTS db_country_taxation;
+
+CREATE TABLE `db_country_taxation` (
+  `ID` decimal(25,0) NOT NULL COMMENT '国税数据主键ID',
+  `ENTERPRISE_NAME` varchar(300) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '国税数据企业名称',
+  `ENTERPRISE_CODE` varchar(50) DEFAULT NULL COMMENT '统一社会信用代码',
+  `TAXATION_DATA` decimal(18,0) DEFAULT NULL COMMENT '实际缴纳国税金额（万元）',
+  `TAXATION_DETAIL` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT'国税税费明细',
+  `USER_ID`   decimal(18,0) DEFAULT NULL COMMENT '记录导入用户ID',
+  `CREATE_DATE` datetime DEFAULT NULL COMMENT '记录首次创建的时间。',
+  `UPDATE_DATE` datetime DEFAULT NULL COMMENT '记录每次修改的时间。',
+  `REMARK` varchar(2000) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '记录备注。',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `idx_enterprise_code` (`ENTERPRISE_CODE`),
+  KEY `idx_enterprise_name_createtime` (`CREATE_DATE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='记录企业国税数据资源数据信息';
