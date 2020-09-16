7179849494387b9891788f4bedef3fe057d67e62
chenjun <chenjun8@asiainfo.com>
2020-09-16 16:36:31 +0800

    vueCesium

---
+++ vueCesium/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+var Cesium = require('cesium/Cesium');
+var widgets= require('cesium/Widgets/widgets.css');
+
+Vue.config.productionTip = false
+Vue.prototype.Cesium = Cesium;
+Vue.prototype.widgets = widgets;
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
